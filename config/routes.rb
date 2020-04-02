48d0500b4e29c7ad98d6df01fc033f9a3d70865f
github username <harr.hughes@gmail.com>
2020-04-02 16:40:23 +0100

    githubuser changed to resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :git_hub_users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "user#index"
+  get '/find', to: 'user#new' 
+
 end
--- test/models/git_hub_user_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GitHubUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
